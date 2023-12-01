ea5772484b8abf6e5b6133188d76735eacec3b4d
kumarAjay10 <144784190+kumarAjay10@users.noreply.github.com>
2023-12-01 19:30:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+
+	for(i=1; i<=100;i++){
+	
+	if(i%3===0){
+		alert("Fizz");
+	}
+	else if(i%5===0){
+		alert("Buzz");
+	}
+	else if(i%3===0 && i%5===0){
+		alert("FizzBuzz");
+	}
+	else{
+		alert(i);
+	}
+	
+	}
+
